9eda7642222b631de5e8d21e39be2bd6b934b941
CamilaNieto-Centennial <cnietomo@my.centennialcollege.ca>
2021-12-04 16:35:56 -0500

    .gitignore updated

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+//COMP 229-005 Assignment 2
+//Author: Camila Nieto
+//ID: 301175110
+module.exports = 
+{
+    //"URI": "mongodb://localhost:27017/portfolio",
+    "URI": "mongodb+srv://CamilaNieto:Patacamilo4444@cluster0.efjyw.mongodb.net/portfolio?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
